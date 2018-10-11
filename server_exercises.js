34709508aedebf89063fcda423c684d45d58738d
AGCH <anastasiyache@gmail.com>
2018-10-11 18:06:03 +0300

    Sequelize 2ex-s

--- server_exercises.js
+++ server_exercises.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://sql7260491:77E6J8Rgwr@sql7.freesqldatabase.com:3306/sql7260491');
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    })
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect to the database:', err);
+//     })
     const Movie = sequelize.define('movie', {
         m_name: {
             type: Sequelize.STRING
@@ -30,12 +30,11 @@ sequelize
     Actor.belongsToMany(Movie, { through: Movie_Actor })
     Movie.belongsToMany(Actor, { through: Movie_Actor })
     
-    // Director.sync()
-    // Actor.sync()
-    // Movie_Actor.sync()
-    // Movie.sync()
+    // Director.sync();
+    // Actor.sync();
+    // Movie.sync();
+    // Movie_Actor.sync();
 
-    // Movie.sync();Actor.sync();Director.sync();
     // Actor.create({
     //         a_name: "Ian McKellen"
     //     })
@@ -49,15 +48,35 @@ sequelize
     // })
        
     const addRelationship = async function () {
-        let movie = await Movie.create({ m_name: "Lord of the Lions", directorId: 2})
+        let movie = await Movie.create({ m_name: "Lion King", directorId: 1})
         let actor = await Actor.create({ a_name: "Jeremy Irons" })
-        movie.addArtist(actor)
+        movie.addActor(actor)
         Director.find({
-            where: { id: 2 },
-            include: [Movie]
-        })
-        //alternatively, could also do artist.addMovie(movie)
+        where: { id: 2 },
+        include: [Movie]
+    })
+        // alternatively, could also do artist.addMovie(movie)
     }
-    
     addRelationship()
+   
+    
+    //#3 Find all the movies directed by Peter Jackson
+    // Director.find({
+    //     include: [Movie],
+    //     where: {d_name: "Peter Jackson" }
+        
+    //   })
+    //   .then(results => 
+    //     results.movies.forEach(movie => {
+    //         console.log(movie.dataValues.m_name) 
+    //     }));
+    //#4 Find all the movies in which Ian McKellen played
+    // Movie.find({
+    //     // include: [Movie],
+    //     where: {actorId: 7 }
+    //   })
+    //   .then(results => 
+    //    console.log(results));
+       
+
    
